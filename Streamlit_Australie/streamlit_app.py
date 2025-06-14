b5d1a57207e43a3bfda7876971b5aaae6a1196fc
Amelie-Bo <amelie.bouillaud@wanadoo.fr>
2025-06-14 13:56:24 +0200

    Initial commit after cleanup

---
+++ Streamlit_Australie/streamlit_app.py
@@ -0,0 +1,40 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+df=pd.read_csv("data_2024-25.csv")
+
+st.title("Projet de classification binaire sur la pluie en Australie")
+st.sidebar.title("Sommaire")
+pages=["Exploration", "DataVizualization", "Modélisation"]
+page=st.sidebar.radio("Aller vers", pages)
+
+if page == pages[0] : #Sur la page Exploration
+  st.write("### Introduction")
+  st.dataframe(df.head(10))
+  st.write(df.shape) #equivalent de print
+  st.dataframe(df.describe()) #st.dataframe pour appeler des méthodes pandas qui entraine un affichage de df
+  
+  if st.checkbox("Afficher les NA") : #quand on coche la case, on affiche la méthode ci-dessous
+    st.dataframe(df.isna().sum()) 
+
+
+if page == pages[1] : 
+  st.write("### DataVizualization")
+  #Afficher un graphique de la variable cible "Plui demain"
+  fig = plt.figure()
+  sns.countplot(x = 'RainTomorrow', data = df)
+  st.pyplot(fig)
+  # Analyse multivariée par matrice de corrélation
+  fig, ax = plt.subplots()
+  sns.heatmap(df.corr(), ax=ax)
+  st.write(fig)
+
+
+if page == pages[2] : 
+  st.write("### Modélisation")# sur X_test
+
+  # Supprimer les variables inutiles
+  df = df.drop(['Evaporation', 'Sunshine'], axis=1) 
