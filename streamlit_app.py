9f1366d9ab3f568e32ea3cca1c25162e7a9ccd94
Amelie-Bo <you@example.com>
2025-06-18 13:04:49 +0000

    MàJ dossier Streamlit_AustralieAjout depuis Google Colab

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,25 +8,30 @@ import os
 
 # 1. Charger les données
 df = pd.read_csv("data/weatherAUS.csv")
+X_test = pd.read_csv("data/data_2024-25.csv")
 
-# Load X_test with error handling
-try:
-    df_new_data = pd.read_csv("data/data_2024-25.csv") #Essayer 2024_25 pour voir si ok
-    st.success("data_2024-25.csv loaded successfully!")
-except FileNotFoundError:
-    st.error("Error: data_2024-25.csv not found. Please ensure the file is in the correct directory.")
-except Exception as e:
-    st.error(f"An error occurred while loading data_2024-25.csv: {e}")
-
-# 2. Définir la structure   
-st.title("Projet de classification binaire sur la pluie en Australie")
+# 2. Définir la structure
+st.title("Projet de classification binaire sur la pluie en Australie") # sera répercuté sur toutes les pages du Streamlit
 st.sidebar.title("Sommaire")
 pages=["Exploration", "DataVizualization", "Modélisation","Evaluation"]
 page=st.sidebar.radio("Aller vers", pages)
 
-# 3. Sur la page Exploration 
-if page == pages[0] : 
-  st.write("### Introduction")
+# 3. Sur la page Exploration
+if page == pages[0] :
+  st.header("Introduction") 
+  st.write("Ce projet traite... ") #\n n'apporte rien autant faire un nouveau st.write
+  
+  # test visuel
+  st.title("st.titre") #Gras, 44px
+  st.header("st.header") #36px semi gras, comme ##
+  st.subheader("st.subheader") #28px semi gras, comme ### 
+  st.write("#### 4#..") #24px semi gras 
+  st.write("##### 5#..") #20px semi gras 
+  st.write("###### 6#..") #16px semi gras 
+  st.write("normal") #16px normal (comme un print dans une jupyer NB)
+  st.markdown("Texte **gras**, *italique*, et un [lien](https://example.com)") #pour mettre en forme
+  
+
   st.dataframe(df.head(10))
   st.write(df.shape) #equivalent de print
   st.dataframe(df.describe()) #st.dataframe pour appeler des méthodes pandas qui entraine un affichage de df
@@ -34,20 +39,36 @@ if page == pages[0] :
   if st.checkbox("Afficher les NA") : #quand on coche la case, on affiche la méthode ci-dessous
     st.dataframe(df.isna().sum())
 
-# 3. Sur la page Datavizualisation 
+# 3. Sur la page Datavizualisation
 #Sur le fichier source? sur le nouveau jeu de données?
 if page == pages[1] :
-  st.write("### DataVizualization") 
+  st.write("### DataVizualization")
   #Afficher un graphique de la variable cible "Pluie demain"
   fig = plt.figure()
   sns.countplot(x = 'RainTomorrow', data = df)
   st.pyplot(fig)
+  
+  # Impact de features sur la variable cible
+  fig1 = plt.figure()
+  sns.countplot(x = 'RainTomorrow', hue='RainToday', data = df, title = "lien Variable cible et RainToday")
+  st.pyplot(fig1)
+  
+  fig2 = sns.catplot(x='Cloud3pm', y='RainTomorrow', data=df, kind='point')
+  st.pyplot(fig2)
+  
+  fig3 = sns.lmplot(x='Temp3pm', y='RainTomorrow', hue="Cloud3pm", data=df)
+  st.pyplot(fig3)
+
   # Analyse multivariée par matrice de corrélation
   fig, ax = plt.subplots()
   sns.heatmap(df.corr(), ax=ax)
   st.write(fig)
 
-# 4. Sur la page Modélisation 
+  #Plotly
+  # fig4 = px.scatter(df, x=, y=, title="")
+  st.plotly_chart(fig4) #ADD
+
+# 4. Sur la page Modélisation
 if page == pages[2] :
   st.write("### Modélisation")# sur X_test preprocesse ou non?(mon preprocessing + modelisationprend qq minutes )
 
