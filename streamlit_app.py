011d50aa4761760ec508a0e6b12364ec83c93fa9
Amelie-Bo <you@example.com>
2025-06-18 21:11:03 +0000

    MàJ dossier Streamlit_AustralieAjout depuis Google Colab

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,4 @@
-# Contenu de streamlit_app.py
-
+#-------------------------------------------------------------------------------------------------------------------------------------------------------------------
 ## Chargement des librairies
 import streamlit as st
 
@@ -21,26 +20,32 @@ from sklearn.decomposition import PCA
 from imblearn.metrics import classification_report_imbalanced 
 import xgboost as xgb
 
+import streamlit_shap
+from streamlit_shap import st_shap
+import shap
+
 import pickle
 from joblib import load
 import cloudpickle
 
 import os
+import time
+import requests
+from io import StringIO
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
 
-import streamlit_shap
-from streamlit_shap import st_shap
-import shap
-
+#-------------------------------------------------------------------------------------------------------------------------------------------------------------------
 # 1. Charger les données
 df = pd.read_csv("data/weatherAUS.csv")
 X_test = pd.read_csv("data/data_2024-25.csv")
-
+#-------------------------------------------------------------------------------------------------------------------------------------------------------------------
 # 2. Définir la structure
 st.title("Projet de classification binaire sur la pluie en Australie") # sera répercuté sur toutes les pages du Streamlit
 st.sidebar.title("Sommaire")
-pages=["Exploration", "DataVizualization", "Modélisation","Evaluation"]
+pages=["Exploration", "DataVizualization", "Modélisation","Evaluation","Datas actuelles"]
 page=st.sidebar.radio("Aller vers", pages)
-
+#-------------------------------------------------------------------------------------------------------------------------------------------------------------------
 # 3. Sur la page Exploration
 if page == pages[0] :
   st.header("Introduction") 
@@ -63,11 +68,11 @@ if page == pages[0] :
 
   if st.checkbox("Afficher les NA") : #quand on coche la case, on affiche la méthode ci-dessous
     st.dataframe(df.isna().sum())
-
+#-------------------------------------------------------------------------------------------------------------------------------------------------------------------
 # 3. Sur la page Datavizualisation
 #Sur le fichier source? sur le nouveau jeu de données?
 if page == pages[1] :
-  st.write("### DataVizualization")
+  st.header("DataVizualization")
   #Afficher un graphique de la variable cible "Pluie demain"
   fig = plt.figure()
   sns.countplot(x = 'RainTomorrow', data = df)
@@ -92,13 +97,152 @@ if page == pages[1] :
   #Plotly
   # fig4 = px.scatter(df, x=, y=, title="")
   st.plotly_chart(fig4) #ADD
-
+#-------------------------------------------------------------------------------------------------------------------------------------------------------------------
 # 4. Sur la page Modélisation
 if page == pages[2] :
-  st.write("### Modélisation")# sur X_test preprocesse ou non?(mon preprocessing + modelisationprend qq minutes )
+  st.header("Modélisation")# sur X_test preprocesse ou non?(mon preprocessing + modelisationprend qq minutes )
 
   # Supprimer les variables inutiles
   X_test = X_test.drop(['Evaporation', 'Sunshine'], axis=1)
-
+#-------------------------------------------------------------------------------------------------------------------------------------------------------------------
 if page == pages[3] :
-  st.write("### Evaluation") #
+  st.header("Evaluation") 
+#-------------------------------------------------------------------------------------------------------------------------------------------------------------------
+if page == pages[4] :
+  st.header("Datas actuelles") 
+#-1. Collecte des données actuelles---------------------------------------------------------------------------------------------------------------------------------
+  #1.1 Initialisation
+  ##1.1.1 Liste des mois
+  mois_courant = datetime.now().replace(day=1)
+  mois_depart = mois_courant - relativedelta(months=1) # Mois de départ = mois précédent
+  # Générer les 13 mois vers le passé (de -12 à 0 mois avant mois_depart)
+  liste_mois_a_selectionner = [(mois_depart - relativedelta(months=i)).strftime("%Y%m") for i in reversed(range(13))]
+  
+  ##1.1.2 Dico stations
+  #with open("dico scaler/dico_station.pkl", "wb") as fichier:
+  #  pickle.dump(dico_stations_updated, fichier)
+  dico_stations_DWO_a_selectionner = {
+    "IDCJDW5001": ('Adelaide Airport', '023034', 'Adelaide', 'Note 1', 23, 'South Australia'),
+    "IDCJDW6002": ('Albany', '009999', 'Albany', 'Note 1', 9, 'Western Australia'),} 
+
+  ##1.1.3 Noms des colonnes dans le df généré (futur X_test)
+  nom_colonnes_df_principal = {"Minimum temperature (°C)" : "MinTemp", 
+                             "Maximum temperature (°C)": "MaxTemp",
+                             "Rainfall (mm)" : "Rainfall", 
+                             "Evaporation (mm)" : "Evaporation", 
+                             "Sunshine (hours)" :"Sunshine",
+                             "Direction of maximum wind gust ":"WindGustDir", 
+                             "Speed of maximum wind gust (km/h)" : "WindGustSpeed",
+                             "Time of maximum wind gust" : "Time of maximum wind gust" ,  #nouvelle colonne
+                             "9am Temperature (°C)": "Temp9am",
+                             "9am relative humidity (%)" : "Humidity9am", 
+                             "9am cloud amount (oktas)":"Cloud9am",
+                             "9am wind direction" :"WindDir9am", 
+                             "9am wind speed (km/h)": "WindSpeed9am", 
+                             "9am MSL pressure (hPa)":"Pressure9am",
+                             "3pm Temperature (°C)":"Temp3pm", 
+                             "3pm relative humidity (%)":"Humidity3pm",
+                             "3pm cloud amount (oktas)": "Cloud3pm", 
+                             "3pm wind direction" : "WindDir3pm",
+                             "3pm wind speed (km/h)": "WindSpeed3pm", 
+                             "3pm MSL pressure (hPa)":"Pressure3pm"}
+
+  # 1.2 Saisie utilisateur
+  st.subheader("Sélection ")
+  
+  liste_mois = st.selectbox("Sélectionnez un mois", liste_mois_a_selectionner)
+
+  ## 1.2.1 Afficher le nom des stations dans la liste déroulante
+  # Multiselect avec affichage du nom de la station
+  stations_selectionnees = st.multiselect(
+      "Sélectionnez une ou plusieurs stations",
+      options=list(dico_stations_DWO_a_selectionner.keys()),
+      format_func=lambda x: dico_stations_DWO_a_selectionner[x][0])
+  # Générer un dictionnaire filtré identique en format à l’original
+  dico_stations_DWO = {
+      k: dico_stations_DWO_a_selectionner[k]
+      for k in stations_selectionnees}
+
+  # 1.3 Code récupérant les csv et les conslidant dans le Df df_conso_station à partir d'une liste d'url
+  compteur = 0 #pour consolider les df de station dans un un seul df : df_conso_station
+  df_conso_station=pd.DataFrame()
+
+  for no_report in dico_stations_DWO :
+      i=0
+      compteur+= 1
+      df_une_station=pd.DataFrame()
+
+      for le_annee_mois in liste_mois :
+          url_concatene = ("http://www.bom.gov.au/climate/dwo/"+str(le_annee_mois)+"/text/"+no_report+"."+le_annee_mois+".csv") # Exemple : http://www.bom.gov.au/climate/dwo/202412/text/IDCJDW2804.202412.csv
+          i+= 1
+          # Essayer de télécharger le fichier avec des en-têtes de type navigateur
+          headers = {
+              'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
+          }
+
+          # Effectuer la requête
+          response = requests.get(url_concatene, headers=headers)
+
+          # Vérifier si la requête est réussie
+          if response.status_code == 200:
+              # Utiliser StringIO pour lire le texte CSV dans un DataFrame
+              csv_data = StringIO(response.text)
+              
+              # L'entete n'est a=pas toujours sur la meme ligne : On lit le fichier ligne par ligne pour trouver le header qui commence par ,"Date", 
+              lines = response.text.splitlines()
+              # Trouver la ligne où "Date" apparaît pour la première fois
+              header_row = None
+              for i, line in enumerate(lines):
+                  if "Date" in line:
+                      header_row = i
+                      break
+              
+              df_recupere = pd.read_csv(csv_data, sep=",", skiprows=header_row, encoding="latin1")
+              st.dataframe(df_recupere.head(10)) #Ne s'affiche pas?
+              
+              #Faire un df consolidé par station
+              if i == 1 :
+                  df_une_station = df_une_station
+              else :
+                  df_une_station = pd.concat([df_une_station, df_recupere], ignore_index=True) 
+          
+          else:
+              st.write("Erreur lors du chargement de l'URL pour {} de {} : {}".format(le_annee_mois, dico_stations_DWO[no_report][2], response.status_code))
+      
+      #Mettre les noms de colonnes du df principal
+      df_une_station = df_une_station.rename(nom_colonnes_df_principal, axis = 1) 
+      st.dataframe(df_une_station.head(10)) #temp
+      #Suppression de colonnes
+      df_une_station = df_une_station.drop(["Unnamed: 0","Time of maximum wind gust"],axis =1)
+
+      #Ajout de colonnes
+      #Insérer en 2e position (loc=1) le nom de la station (3e colonne du dico) associé à ce rapport dans le dictionnaire dico_stations_DWO
+      df_une_station.insert(1,column="Location", value=dico_stations_DWO[no_report][2]) 
+
+      df_une_station["RainToday"]=df_une_station["Rainfall"].apply(lambda x: "Yes" if x>1 else "No")
+      #Met les valeurs de RainToday à l'indice précédent dans RainTomorrow 
+      df_une_station["RainTomorrow"] = np.roll(df_une_station["RainToday"].values, 1) #Ex RainToday 02/01/24 -> RainTomorrow 01/01/2024
+      
+      #Supprimer le dernier relevé du df car RainTomorrow y sera toujours inconnu (suite au np.roll c'est la valeur de RainToday a la 1e ligne du df, et il aurait fallu celle du lendemain de la dernier ligne du df).
+      df_une_station.drop(df_une_station.index[-1], inplace=True)
+
+      #Mettre les colonnes dans le même ordre que le df de départ du projet
+      df_reference=pd.read_csv("weatherAUS.csv",parse_dates=["Date"])
+      df_une_station = df_une_station[df_reference.columns]
+
+
+      #Faire un df consolidé (df_conso_station) des df unitaire par station (df_une_station)
+      if compteur == 1 :
+          df_conso_station = df_une_station
+      else :
+          df_conso_station = pd.concat([df_conso_station, df_une_station], ignore_index=True) 
+  X_test = df_conso_station
+
+  # 1.4 Preprocessing de base
+  st.dataframe(X_test.head(10))
+
+  # 2.1 Sélectionner un jour
+
+  # 2.2 reste du preprocessing (Date supprimée) ##stop ici
+
+  # 3 Prediction 
